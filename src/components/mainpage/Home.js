ec8810b3f9064d40542a20073d9f601f8f81127e
moolasubbaramireddy <subbaramireddym@gmail.com>
2022-10-10 22:36:19 +0530

    home lo login button get start button ga set cheshena

--- src/components/mainpage/Home.js
+++ src/components/mainpage/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
       <div className="bg ">
           <div className="body">
             <h1 className="uppercase text-2xl">welcome to my world </h1> 
-            <Link to='/login'> <button className="uppercase border border-lime-400 p-1 m-2  rounded-full  font-semibold">log in</button></Link>
+            <Link to='/products'> <button className="uppercase border border-lime-400 p-1 m-2  rounded-full  font-semibold">get start</button></Link>
           </div>
           
       </div>
